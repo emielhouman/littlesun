a776874ebe564da9231a27627fc60fc5962298fc
emielhouman <emielhouman@gmail.com>
2024-04-30 18:38:53 +0200

    changed the login page to make it more object oriented with classes

--- login.php
+++ login.php
@@ -1,31 +1,22 @@
-<?php 
-
-function canLogin($pEmail, $pPassword) {
-	$connection = new mysqli("localhost", "root", "root", "littlesun");
-	$pEmail = $connection->real_escape_string($pEmail);
-	$sql = "SELECT password FROM users WHERE email = '$pEmail'";
-	$result = $connection-> query($sql);
-	$user = $result->fetch_assoc();
-	if(password_verify($pPassword, $user['password'])){
-		return true;
-	} else {
-		return false;
-	}
-};
+<?php
+include_once(__DIR__ . "/bootstrap.php");
 
 if (!empty($_POST)) {
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	if (canLogin($email, $password)) {
-		session_start();
-		$_SESSION['loggedin'] = true;
-		header('Location: index.php');
-	} else {
-		$error = true;
-	}
-}
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-?><!DOCTYPE html>
+    $user = new User;
+    $user->setEmail($email);
+    $user->setPassword($password);
+
+    if ($user->canLogin()) {
+        header('Location: index.php');
+    } else {
+        $error = true;
+    }
+}
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -39,7 +30,7 @@ if (!empty($_POST)) {
         <form action="" method="post">
             <h2>Sign In</h2>
             <?php if (isset($error)) : ?>
-                <div class="form__error">
+                <div style="color: red;">
                     <p>Something went wrong... Please try again!</p>
                 </div>
             <?php endif; ?>
@@ -53,7 +44,6 @@ if (!empty($_POST)) {
             </div>
             <div class="form__field">
                 <input type="submit" value="Sign up" class="button">
-                <input type="checkbox" id="rememberUser"><label for="rememberUser">Remember me</label>
             </div>
         </form>
     </div>
