f7d224717e58e2560f782a19bb6d06a40ab3d6d4
emielhouman <emielhouman@gmail.com>
2024-04-30 18:29:02 +0200

    created an account page

---
+++ account.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Little Sun</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="flex">
+        <?php include_once(__DIR__ . "/nav.inc.php") ?>
+        <div class="mx-14 my-10">
+            <h2 class="font-bold text-3xl pt-1">Hello World!</h2>
+        </div>
+    </div>
+</body>
+
+</html>
