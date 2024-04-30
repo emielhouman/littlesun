16cfbb119b97bc66af909416a002ca6adeff5e85
emielhouman <emielhouman@gmail.com>
2024-04-30 18:45:53 +0200

    created a location page for the details of a location + editting the location

--- location.php
+++ location.php
@@ -1,56 +1,68 @@
 <?php
-$conn = new PDO('mysql:host=localhost;dbname=littlesun', "root", "root");
+include_once(__DIR__ . "/bootstrap.php");
 
-// Check if the delete form is submitted
-if(isset($_POST['delete'])) {
-    $id = $_POST['id'];
-    $statement = $conn->prepare("DELETE FROM locations WHERE id = :id");
-    $statement->bindValue(":id", $id);
-    $statement->execute();
-    // Redirect to prevent form resubmission
-    header("Location: location.php");
+if (isset($_GET['id']) && isset($_GET['mode'])) {
+    $managers = Manager::getAll();
+    $location = Location::getLocationInfo($_GET['id']);
+
+    if ($_GET['mode'] === "edit" && !empty($_POST)) {
+        $id = $location['id'];
+        $name = $_POST['location'];
+        $manager = $_POST['manager'];
+
+        Location::updateLocation($id, $name, $manager);
+
+        header("Location: location.php?id=$id&mode=read");
+        exit;
+    } else if ($_GET['mode'] === "read" && !empty($_POST)) {
+        header("Location: locations.php");
+        exit;
+    }
+} else {
+    header("Location: locations.php");
     exit;
 }
-
-// Insert new location
-if (!empty($_POST)) {
-    $location = $_POST["location"];
-    $statement = $conn->prepare("INSERT INTO locations (location) VALUES (:location)");
-    $statement->bindValue(":location", $location); 
-    $statement->execute();
-}
-
-// Fetch locations from the database
-$statement = $conn->prepare("SELECT * FROM locations");
-$statement->execute();
-$locations = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Little Sun</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <form action="" method="post">
-        <div class="form__field">
-            <label for="Location">Create new Hub Location</label>
-            <input type="text" name="location">
-            <input type="submit" value="add" class="btn btn--primary"> 
-        </div>
-    </form>
 
-    <ul>
-        <?php foreach ($locations as $location): ?>
-            <li><?php echo htmlspecialchars($location['location']) ?>
-                <form action="" method="post" style="display: inline;">
-                    <input type="hidden" name="id" value="<?php echo $location['id']; ?>">
-                    <button type="submit" name="delete">Delete</button>
-                </form>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+<body>
+    <div class="flex w-full">
+        <?php include_once(__DIR__ . "/nav.inc.php") ?>
+        <div class="flex-1 mx-16 my-10">
+            <div class="flex justify-between items-end pt-1">
+                <h2 class="font-bold text-3xl">HUB Location: <span class="font-normal"><?php echo $location['name'] ?></span></h2>
+                <a href="location.php?id=<?php echo $location['id'] ?>&mode=<?php echo ($_GET['mode'] === "edit" ? "read" : "edit") ?>" class="px-2 py-0.5 font-bold rounded border-2 cursor-pointer <?php echo ($_GET['mode'] === "edit" ? "bg-gray-200 border-gray-200" : "bg-yellow-400 border-yellow-400") ?>">
+                    <?php echo ($_GET['mode'] === "edit" ? "Undo changes" : "Edit location") ?>
+                </a>
+            </div>
+            <form class="w-full grid grid-cols-2 gap-x-14 gap-y-6 pt-16 px-12" action="" method="post">
+                <div>
+                    <label class="block py-1 font-semibold text-lg" for="location">Location:</label>
+                    <input class="w-full h-12 px-2.5 text-lg rounded border-2 <?php echo ($_GET['mode'] === "edit" ? "bg-white border-gray-300" : "bg-gray-100 border-gray-300") ?>" id="location" name="location" type="text" value="<?php echo $location["name"] ?>" <?php echo ($_GET['mode'] === "edit" ? "" : "disabled") ?>>
+                </div>
+                <div>
+                    <label class="block py-1 font-semibold text-lg" for="manager">Manager:</label>
+                    <select class="w-full h-12 px-1.5 text-lg opacity-100 rounded border-2 <?php echo ($_GET['mode'] === "edit" ? "bg-white border-gray-300" : "bg-gray-100 border-gray-300") ?>" id="manager" name="manager">
+                        <option value="0">No manager selected</option>
+                        <?php foreach ($managers as $manager) : ?>
+                            <option value="<?php echo $manager['id'] ?>" <?php echo ($location['manager_id'] === $manager['id'] ? 'selected' : '') ?>><?php echo $manager['firstname'] . " " . $manager['lastname'] ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="pt-8 col-span-2">
+                    <input class="w-full h-12 font-bold uppercase rounded border-2 cursor-pointer <?php echo ($_GET['mode'] === "edit" ? "bg-yellow-400 border-yellow-400" : "bg-gray-200 border-gray-200") ?>" type="submit" value="<?php echo ($_GET['mode'] === "edit" ? "Update location" : "All locations") ?>">
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
