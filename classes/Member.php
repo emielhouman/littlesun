d055a69ccc51d19181b744adc98c9a9c5df3f50b
ThibaultRondelez <thibault.rondelez@outlook.com>
2024-05-03 19:35:18 +0200

    added a public function getUsersWithRoleId to select specific users

--- classes/Member.php
+++ classes/Member.php
@@ -2,7 +2,18 @@
 
 include_once(__DIR__ . "/Db.php");
 
-class Manager extends User
+class Member extends User
 {
+    
+        public function getUsersWithRoleId($roleId)
+        {
+            $conn = Db::getConnection();
+            $statement = $conn->prepare("SELECT id, firstname, lastname FROM users WHERE role_id = :role_id");
+            $statement->bindValue(":role_id", $roleId);
+            $statement->execute();
+            $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+            return $users;
+        }
     
 }
